a6625d75c57b0fc82d6b37b02fcafae1073f3fe6
Pedro Henrique <vazpedro06@gmail.com>
2024-10-23 18:20:35 -0300

    atividade Selection Sort

--- atividades/questao24.ts
+++ atividades/questao24.ts
@@ -1,8 +1,23 @@
-const arr = [2, 1, 5, 7, 3, 9, 11]
+const arr = [5, 7, 3, 9, 11, 24, 8, 90, 88, 79, 120, 30, 2, 1, 0]
+let n = arr.length
 
- function selectionSort (arr: number[]) {
-    let menorParaMaior: number = arr[0]
-    for (const organizer of arr ){
-    
-    }
+function selectionSort (arr: number[]) {
+    for (let i = 0; i < n; i++) { // Iterando sobre o array
+        let numIndex = i // Armazenando o primeiro elemento
+        for (let j = 0; j < n; j++){ // Segunda iteração 
+            if (arr[j] < arr[numIndex]){ // A cada iteração e verificado se o elemento do array J e menor que o elemento do numindex
+                numIndex = j // Caso seja menor, atualizamos o numIndex e ele passará a ser o elemento de menor valor                    
+            }
+        }
+        if (numIndex !== i){ // se for verdadeiro, encontramos um valor diferente de I
+            let aux = arr[i] // guarda o valor do arr I em auxiliar
+            arr[i] = arr[numIndex] // arr I recebe um novo valor (arr NumIndex = menor valor)
+            arr[numIndex] = aux // arr de NumIndex recebe o valor de aux (valor antigo)
+        }
+    } 
+    return arr // retorna o arr ordenado
 }
+const sorted = selectionSort(arr)
+console.log(sorted)
+
+// depois de ordenado, nao precisa se preocupar com oque tive para tras
