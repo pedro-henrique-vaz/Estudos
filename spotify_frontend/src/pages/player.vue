ec0ae353a9de879d8b1dd4093a01b810ae3c8136
Pedro Henrique <vazpedro06@gmail.com>
2025-08-21 17:30:27 -0300

    projeto spotify

---
+++ spotify_frontend/src/pages/player.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="music-container">
+      <h4 id="playlist-title">As Melhores Músicas Estão Aqui</h4>
+      <img id='cover' alt="Disc Cover." src="http://192.168.1.108:1212/src/Coldplay/Moon%20Music/moon_music_cover.jpg"/>
+      <audio id='audio'></audio>
+      <div id="below-cover">
+        <div id="song-info">
+          <div id="song-name">Nome da música</div>
+          <div id="band-name" class="light-color">Nome da banda</div>
+        </div>
+        <button id='like' class="button light-color"><i class="bi bi-heart"></i></button>
+      </div>
+      <div id="progress-container">
+        <div id="progress-bar">
+          <div id="current-progress"></div>
+        </div>
+        <div id="time-box">
+          <div id="song-time">00:00:00</div>
+          <div id="total-time">00:00:00</div>
+        </div>
+      </div>
+      <div id="button-container">
+        <button id="shuffle" class="button"><i class="bi bi-shuffle"></i></button>
+        <button id="previous" class="button button-navigate"><i class="bi bi-skip-start-fill"></i></button>
+        <button id="play" class="button button-biggest"><i class="bi bi-play-circle-fill"></i></button>
+        <button id="next" class="button button-navigate"><i class="bi bi-skip-end-fill"></i></button>
+        <button id="repeat" class="button"><i class="bi bi-repeat"></i></button>
+      </div>
+    </div>
+    <div>
+      <input type="text" id="search" placeholder="O que vocẽ quer ouvir?"/>
+    </div>
+</template>
+
+<script lang="ts" setup>
+  
+</script>
